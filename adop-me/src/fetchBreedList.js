46698c8ed9385e004995b767043d5c2652dcc61b
Alex Bravo <abravo@abravo-mac.local>
2024-03-06 15:31:51 -0500

    frontend-Master-5: Capabilities - React-query updating refactoring useBreedList to use react-query

---
+++ adop-me/src/fetchBreedList.js
@@ -0,0 +1,20 @@
+const fetchBreeList = async ({ queryKey }) => {
+    const animal = queryKey[1];
+
+    if ( !animal ){
+        return [];
+    }
+
+    const apiResp = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
+    //is good idea to handle any possible error here.
+    if (!apiResp.ok) {
+      throw new console.error(`breeds/${animal} fetch no ok`);
+    }
+    // it needs to return a promise.
+    // common way to do it is by this:
+    // return await apiResp.json() basicaly the same, just few mileconds more spensive.
+    return apiResp.json();
+  };
+  
+  export default fetchBreeList;
+  
--- adop-me/src/useBreedList.js
+++ adop-me/src/useBreedList.js
@@ -1,32 +1,45 @@
-import { useState, useEffect } from "react";
+//Moving to use react-query instead.
 
-const breedListCache = {};
+import { useQuery } from "@tanstack/react-query";
+import fetchBreeList from "./fetchBreedList";
 
-export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("inloaded");
-
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (breedListCache[animal]) {
-      setBreedList(breedListCache[animal]);
-    } else {
-      requestBreedList();
-    }
-
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const response = await res.json();
-      breedListCache[animal] = response.breeds || [];
-      setBreedList(breedListCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
-
-  return [breedList, status];
+export default function useBreedList ( animal ) {
+  const result = useQuery(["breed", animal], fetchBreeList);
+  //new ternary condition in JS
+  return [result?.data?.breeds ?? [], result.status];
 }
+
+
+
+// import { useState, useEffect } from "react";
+
+// const breedListCache = {};
+
+// export default function useBreedList(animal) {
+//   const [breedList, setBreedList] = useState([]);
+//   const [status, setStatus] = useState("inloaded");
+
+//   useEffect(() => {
+//     if (!animal) {
+//       setBreedList([]);
+//     } else if (breedListCache[animal]) {
+//       setBreedList(breedListCache[animal]);
+//     } else {
+//       requestBreedList();
+//     }
+
+//     async function requestBreedList() {
+//       setBreedList([]);
+//       setStatus("loading");
+//       const res = await fetch(
+//         `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+//       );
+//       const response = await res.json();
+//       breedListCache[animal] = response.breeds || [];
+//       setBreedList(breedListCache[animal]);
+//       setStatus("loaded");
+//     }
+//   }, [animal]);
+
+//   return [breedList, status];
+// }
