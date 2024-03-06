8951caa54d567cffe7e17cec9affebc2e6919bf7
Alex Bravo <abravo@abravo-mac.local>
2024-03-06 14:30:05 -0500

    frontend-Master-5: Capabilities - React-query updating details with pets data

--- adop-me/src/Details.jsx
+++ adop-me/src/Details.jsx
@@ -1,8 +1,27 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import FetchPet from "./FetchPet";
 
 const Details = () => {
   const { id } = useParams();
-  return <h2>{id}</h2>;
+  const result = useQuery(["details", id], FetchPet);
+  if( result.isLoading ){
+    return(
+        <div className="loading-pane">
+            <h2 className="loader">🐷</h2>
+        </div>
+    )}
+
+    const pet = result.data.pets[0];
+
+  return (<div><div className="details">
+  <div>
+    <h1>{pet.name}</h1>
+    <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+    <button>Adopt {pet.name}</button>
+    <p>{pet.description}</p>
+  </div>
+</div></div>);
 };
 
 export default Details;
