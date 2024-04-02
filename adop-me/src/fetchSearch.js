98db67156b279673c8a027a950e56456d4aca00d
Alex Bravo <abravo@abravo-mac.local>
2024-04-01 20:32:20 -0500

    UseQuery as a cahce system.

---
+++ adop-me/src/fetchSearch.js
@@ -0,0 +1,14 @@
+async function fetchSearch({ queryKey }){
+    const { animal, location, breed } = queryKey[1];
+    console.log(animal, location, breed
+        )
+    const rest = await fetch(
+        `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`);
+        if(!rest.ok){
+            throw new console.error(`pet search no okay ${animal}, ${location}, ${breed}`);
+        }
+
+        return rest.json();
+}
+
+export default fetchSearch;
