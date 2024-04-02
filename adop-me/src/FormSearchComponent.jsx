34cebd65b8ea11518f014739ab468e3566fca677
Alex Bravo <abravo@abravo-mac.local>
2024-04-01 20:29:12 -0500

    UseQuery as a cahce system.

--- adop-me/src/FormSearchComponent.jsx
+++ adop-me/src/FormSearchComponent.jsx
@@ -1,63 +1,76 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
+import fetchSearch from "./fetchSearch";
 
 const ANIMALS = ["bird", "dog", "cat", "reptile", "rabbit"];
 
 const FormSearchComponent = () => {
-  const [pets, setPets] = useState([]);
-  const [location, setLocation] = useState("");
+  const [requestParams, setRequestParams] = useState({
+    location: "",
+    animal: "",
+    breed: ""
+  });
+  //const [pets, setPets] = useState([]);
+  //const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
-  const [breed, setBreed] = useState("");
   const [breeds] = useBreedList(animal);
+
+  const result = useQuery([ "search", requestParams ], fetchSearch);
+  const pets = result?.data?.pets ?? [];
+
 
   // 1. By default Efects will load each time the componen is re render
   //    to change this behaviurs we can use and aditional parameter where we can set de dependecies.
   //  [] empty means reload only the firts time
   //  [location, animal, breed] means fetch any time the dependencies changes.
-  useEffect(() => {
-    requestPets();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   requestPets();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
-  async function requestPets() {
-    const rest = await fetch(
-      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-    );
-    const json = await rest.json();
-    setPets(json.pets);
-  }
+  // async function requestPets() {
+  //   const rest = await fetch(
+  //     `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+  //   );
+  //   const json = await rest.json();
+  //   setPets(json.pets);
+  // }
 
   return (
     <div className="search-params">
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          requestPets();
+          //Browser API
+          const formData = new FormData(e.target);
+          const obj = {
+            animal : formData.get("animal") ?? "",
+            location : formData.get("location") ?? "",
+            breed : formData.get("breed") ?? "",
+          };
+          setRequestParams(obj);
         }}
       >
         <label htmlFor="location">
           Location
           <input
-            onChange={(e) => setLocation(e.target.value)}
-            id="locatin"
-            value={location}
-            placeholder="Location"
-          ></input>
+          name="location"
+            id="location"
+            placeholder="Location" />
         </label>
-        <label htmlFor="Anilam">
+        <label htmlFor="Animal">
           Animal
           <select
             onChange={(e) => {
               setAnimal(e.target.value);
-              setBreed("");
             }}
             onBlur={(e) => {
               setAnimal(e.target.value);
-              setBreed("");
             }}
             id="animal"
-            value={animal}
+            name="animal"
           >
             <option />
             {ANIMALS.map((animal) => (
@@ -71,15 +84,9 @@ const FormSearchComponent = () => {
         <label htmlFor="Breed">
           Breed
           <select
+          name="breed"
             disabled={!breeds.length}
-            onChange={(e) => {
-              setBreed(e.target.value);
-            }}
-            onBlur={(e) => {
-              setBreed(e.target.value);
-            }}
             id="breed"
-            value={breed}
           >
             <option />
             {breeds.map((breed) => (
